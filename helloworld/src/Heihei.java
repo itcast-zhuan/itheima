359755986aa6e5c8bd710a4db7febe795491f00a
zhuan <zhuan@itcast.cn>
2018-09-01 18:37:06 +0800

    add Heihei 自娱自乐

---
+++ helloworld/src/Heihei.java
@@ -0,0 +1,14 @@
+/**
+ * 作者： zhuan
+ * 日期： 2018/9/1 18:35
+ * 描述：
+ */
+public class Heihei {
+    public static void main(String[] args){
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hahah");
+        System.out.println("hahah");
+    }
+}
